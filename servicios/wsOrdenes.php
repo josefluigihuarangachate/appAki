763ed3d508d891e801e92a1e2321ad86b1945ceb
josefluigihuarangachate <josefluigihuarangachate@gmail.com>
2021-09-21 14:59:06 -0500

    Web service ordenes

---
+++ servicios/wsOrdenes.php
@@ -0,0 +1,76 @@
+<?php
+
+include './include/app.php';
+include './include/session.php';
+
+$generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+
+use Dompdf\Dompdf;
+
+if (METODO($method) == 'GET') {
+
+    if ($cmd == 'listadeorden') {
+
+        $data = $pdo->select(
+                tabla('orden'),
+                [
+                    tabla('orden') . ".id(idorden)",
+                    tabla('orden') . ".idservicio(idservicio)",
+                    tabla('orden') . ".nombre_servicio",
+                    tabla('orden') . ".numeroorden(numeroorden)",
+                    tabla('orden') . ".nombre_repartidor(nombredelrepartidor)",
+                    tabla('orden') . ".fecha(fechaderegistro)",
+                    tabla('orden') . ".hora(horaderegistro)",
+                    tabla('orden') . ".nombre_cliente(nombredelcliente)",
+                    tabla('orden') . ".celular_cliente(celulardelcliente)",
+                    tabla('orden') . ".direccion_cliente(direcciondelcliente)",
+                    tabla('orden') . ".fecha_entrega(fechadeentrega)",
+                    tabla('orden') . ".hora_entrega(horadeentrega)",
+                    tabla('orden') . ".tipo_cobro(tipodecobro)",
+                    tabla('orden') . ".idzona(idzona)", // C1, C2, C3,...
+                    tabla('orden') . ".total_pagar(totalapagar)",
+                    tabla('orden') . ".estado(estadodeorden)",
+                    tabla('orden') . ".tipo_pago(tipodepago)",
+                    tabla('orden') . ".tipo_atencion(tipodeatencion)",
+                    tabla('orden') . ".array_detalleorden(detalledeorden)",
+                    tabla('orden') . ".flag(flag)",
+                ],
+                [
+                    "OR" => [
+                        tabla('orden') . '.flag' => [1, 2]
+                    ]
+                ]
+        );
+
+        if ($data) {
+
+            for ($d = 0; $d < count($data); $d++) {
+                $data[$d]['detalledeorden'] = json_decode($data[$d]['detalledeorden']);
+                continue;
+            }
+
+            $json['code'] = '200';
+            $json['status'] = 'Ok';
+            $json['msg'] = strings('success_read');
+            $json['data'] = $data;
+        } else {
+            $json['msg'] = strings('error_read');
+        }
+    } else {
+        $json['msg'] = strings('error_cmd');
+    }
+} else if (METODO($method) == 'POST') {
+    
+} else if (METODO($method) == 'PUT') {
+    
+} else if (METODO($method) == 'DELETE') {
+    
+} else {
+    $json['msg'] = strings('error_method');
+}
+
+
+//'X-Requested-With', 'XMLHttpRequest'
+header('Content-Type: application/json');
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
+
